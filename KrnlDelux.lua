11fc1d411786f0289f1b45b5a75c3ea6454da6f2
EndersMH-TheReturn <endermh2@gmail.com>
2026-02-24 00:54:56 +0330

    Update KrnlDelux.lua
    A

--- KrnlDelux.lua
+++ KrnlDelux.lua
@@ -124,6 +124,54 @@ highlight.BorderSizePixel = 0
 highlight.Parent = bar
 Instance.new("UICorner", highlight).CornerRadius = UDim.new(1,0)
 
+---------------------------------------------------
+-- DRAG FUNCTION
+---------------------------------------------------
+local dragging = false
+local dragInput
+local dragStart
+local startPos
+
+local function update(input)
+	local delta = input.Position - dragStart
+	bar.Position = UDim2.new(
+		startPos.X.Scale,
+		startPos.X.Offset + delta.X,
+		startPos.Y.Scale,
+		startPos.Y.Offset + delta.Y
+	)
+end
+
+bar.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1
+	or input.UserInputType == Enum.UserInputType.Touch then
+		
+		dragging = true
+		dragStart = input.Position
+		startPos = bar.Position
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+bar.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement
+	or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		update(input)
+	end
+end)
+
+bar.Active = true
 -- ==================== NEW GUI ====================
 local newGui = Instance.new("ImageLabel")
 newGui.Position = UDim2.new(0.5,0,0.5,0)
